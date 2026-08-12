216b8e116c12fb65f93b1054d6640af4c147409d
rexix01 <rexix01@proton.me>
2026-08-12 14:23:06 -0500

    manaus: Configure bootloader board name
    Change-Id: Ib5ef7247dd9ef663f6cee1bee3e58cafbfb3556b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,9 @@
 
 DEVICE_PATH := device/motorola/manaus
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := manaus
+
 # Inherit from mt6879-common
 include device/motorola/mt6879-common/BoardConfigCommon.mk
 
