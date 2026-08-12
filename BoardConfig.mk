028974c74b063c0e0e588d5baf2178d6b4387b41
rexix01 <rexix01@proton.me>
2026-08-12 15:11:13 -0500

    manaus: Set rollback index
    Change-Id: I4bc701c847fb2c83feb6e0d34fcaf44f2fc7d15a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,10 @@ TARGET_BOOTLOADER_BOARD_NAME := manaus
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
+# Verified Boot
+BOARD_AVB_ROLLBACK_INDEX := 30
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := 30
+
 # Inherit from mt6879-common
 include device/motorola/mt6879-common/BoardConfigCommon.mk
 
