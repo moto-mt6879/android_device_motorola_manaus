121955ac9fd09698f5d1dccfe56ae4c2dac1e4a2
rexix01 <rexix01@proton.me>
2026-08-12 15:39:01 -0500

    manaus: Import prebuilt dtbo
    Change-Id: Idd62796b1916fb18327f186b1b2c145dd06957a7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ DEVICE_PATH := device/motorola/manaus
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := manaus
 
+# DTBO
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilts/dtbo.img
+
 # Kernel
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules/modules.load))
 BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules/modules.load.recovery))
