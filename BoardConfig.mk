40a8374fa20d3da941e141cd5af1ac0f04827e73
rexix01 <rexix01@proton.me>
2026-08-12 15:36:54 -0500

    manaus: Configure kernel
    Change-Id: I13ed8ff073b2f3afeddd1ad3050aed99e87aec20

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,12 @@ DEVICE_PATH := device/motorola/manaus
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := manaus
 
+# Kernel
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules/modules.load))
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules/modules.load.recovery))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules/modules.load.vendor_ramdisk))
+BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD) $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD)
+
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
