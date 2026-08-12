3f12b66451b4ae9adf106ee9206d1189c5336060
rexix01 <rexix01@proton.me>
2026-08-12 15:10:25 -0500

    manaus: Configure camera stack
    Change-Id: I553c0276ec937dee6c6a6bf97b658d2fccce25e0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ DEVICE_PATH := device/motorola/manaus
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := manaus
 
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # Inherit from mt6879-common
 include device/motorola/mt6879-common/BoardConfigCommon.mk
 
--- extract-files.py
+++ extract-files.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
 #
-# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-FileCopyrightText: The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
 
 from extract_utils.main import (
     ExtractUtils,
@@ -17,10 +28,31 @@ namespace_imports = [
     'hardware/motorola',
 ]
 
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    ('vendor/lib64/hw/mt6879/android.hardware.camera.provider@2.6-impl-mediatek.so', 'vendor/lib64/mt6879/libmtkcam_stdutils.so'): blob_fixup()
+        .replace_needed('libutils.so', 'libutils-v32.so'),
+    ('vendor/lib64/mt6879/lib3a.flash.so', 'vendor/lib64/mt6879/lib3a.ae.stat.so',
+     'vendor/lib64/mt6879/lib3a.sensors.flicker.so', 'vendor/lib64/mt6879/lib3a.sensors.color.so',
+     'vendor/lib64/lib3a.ae.pipe.so'): blob_fixup()
+        .add_needed('liblog.so'),
+    'vendor/lib64/libstfactory-vendor.so': blob_fixup()
+        .add_needed('libbase_shim.so'),
+}  # fmt: skip
+
 module = ExtractUtilsModule(
     'manaus',
     'motorola',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=True,
 )
 
 if __name__ == '__main__':
